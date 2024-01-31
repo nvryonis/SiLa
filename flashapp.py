562ba633e4cef292bb8b1094990f7e026c6e417b
ghenxuan <132571007+GhenXuan@users.noreply.github.com>
2024-01-31 15:43:49 +0200

    flashapp.py

--- flashapp.py
+++ flashapp.py
@@ -1,28 +1,23 @@
+from gettext import translation
 from flask import Flask, render_template, request, jsonify
 from google.cloud import translate_v2 as translate
 
 app = Flask(__name__)
 
-class Translation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    original_text = db.Column(db.String(255), nullable=False)
-    translated_text = db.Column(db.String(255), nullable=False)
-    target_language = db.Column(db.String(10), nullable=False)
 
-with app.app_context():
-    db.create_all()
 
 @app.route('/')
 def index():
-    translations = Translation.query.all()
+    translations = translation.query.all()
     return render_template('index.html', translations=translations)
 
 @app.route('/translate', methods=['POST'])
 def translate_text():
     data = request.get_json()
     text = data.get('text')
-    target_language = 'el' 
+    target_language = 'el'  # 希腊语的 ISO 639-1 代码
 
+    # 使用 Google Translate API 进行翻译
     client = translate.Client()
     result = client.translate(text, target_language=target_language)
     translated_text = result['input']
